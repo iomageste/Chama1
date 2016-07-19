fdccbe12db6a39f152db41cf0add51e500c7489f
Eduardo Hauck <Eduardo Hauck>
2016-07-19 11:45:35 -0300

    Atualizacoes na interface, menus, etc

--- Chama+1/app/src/main/java/com/example/eduar/chama1/Chama1Fragment.java
+++ Chama+1/app/src/main/java/com/example/eduar/chama1/Chama1Fragment.java
@@ -9,7 +9,11 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -27,9 +31,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class Chama1Fragment extends Fragment{
 
+    Button buttonBuscar;
+    EditText editChamaMais;
+    EditText editTipo;
+    SeekBar seekArea;
+    SeekBar seekComecaEm;
 
     public Chama1Fragment() {
         // Required empty public constructor
+
     }
 
 
@@ -37,7 +47,32 @@ public class Chama1Fragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chama1, container, false);
+        View view = inflater.inflate(R.layout.fragment_chama1, container, false);
+
+        buttonBuscar = (Button) view.findViewById(R.id.buttonBuscar);
+        editChamaMais = (EditText) view.findViewById(R.id.textChamaMais);
+        editTipo = (EditText) view.findViewById(R.id.textTipo);
+        seekArea = (SeekBar) view.findViewById(R.id.seekArea);
+        seekComecaEm = (SeekBar) view.findViewById(R.id.seekComecaEm);
+
+        addListenerOnButton(view);
+
+        return view;
+    }
+
+    public void addListenerOnButton(View view) {
+
+
+        buttonBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String dados = "Chama+"+editChamaMais.getText().toString();
+                dados += ", Tipo:"+editTipo.getText().toString();
+                dados += ", Area:"+seekArea.getProgress();
+                dados += ", ComecaEm:"+seekComecaEm.getProgress();
+                Toast.makeText(getContext(), dados, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
--- Chama+1/app/src/main/java/com/example/eduar/chama1/ConfigFragment.java
+++ Chama+1/app/src/main/java/com/example/eduar/chama1/ConfigFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +17,9 @@ import android.view.ViewGroup;
  */
 public class ConfigFragment extends Fragment {
 
+    Button buttonSalvar;
+    CheckBox checkBoxNotificacao;
+    SeekBar seekArea;
 
     public ConfigFragment() {
         // Required empty public constructor
@@ -23,7 +30,28 @@ public class ConfigFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_config, container, false);
+        View view = inflater.inflate(R.layout.fragment_config, container, false);
+
+        buttonSalvar = (Button) view.findViewById(R.id.buttonSalvar);
+        checkBoxNotificacao = (CheckBox) view.findViewById(R.id.checkBoxNotificacao);
+        seekArea = (SeekBar) view.findViewById(R.id.seekArea);
+
+        addListenerOnButton(view);
+
+        return view;
+    }
+
+    public void addListenerOnButton(View view) {
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String dados = "Notificacao?"+(checkBoxNotificacao.isChecked()?"True":"False");
+                dados += ", Area:"+seekArea.getProgress();
+                Toast.makeText(getContext(), dados, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }

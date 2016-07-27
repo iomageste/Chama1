ac92b255f83a707364b65097518f0fea65dc45e4
Eduardo Hauck <Eduardo Hauck>
2016-07-27 20:59:24 -0300

    Corrigido margem do mapa

--- Chama+1/app/src/main/java/com/example/eduar/chama1/PerfilFragment.java
+++ Chama+1/app/src/main/java/com/example/eduar/chama1/PerfilFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.eduar.model.User;
@@ -21,6 +22,8 @@ import com.firebase.client.Query;
  */
 public class PerfilFragment extends Fragment {
 
+    EditText userName;
+    EditText telefone;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -30,8 +33,35 @@ public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        userName = (EditText) view.findViewById(R.id.textNome);
+        telefone = (EditText) view.findViewById(R.id.textTelefone);
+
+        Firebase myFirebaseRef = new Firebase("https://chama1-e883c.firebaseio.com/");
+        Query queryRef = myFirebaseRef.orderByChild("nome").equalTo("eduardo");
+
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChild) {
+                User user = snapshot.getValue(User.class);
+                if(user != null) {
+                    userName.setText(user.getUsername());
+                    telefone.setText(user.getTelefone());
+                }
+            }
+
+            @Override public void onChildRemoved(DataSnapshot dataSnapshot) { }
+            @Override public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+            @Override public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+            @Override public void onCancelled(FirebaseError firebaseError) { }
+        });
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        return view;
     }
 
 }
